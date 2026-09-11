50deb52285238a83b2ae96a907385a431fa8c28a
root <root@LAPTOP-7IRKDKGA.localdomain>
2026-09-11 21:56:54 +0530

    feat: add MongoDB replica set with 3 pods

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -18,8 +18,8 @@ module "eks" {
     worker-nodes = {
       instance_types = ["t3.large"]
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 4
+      desired_size   = 3
 
       iam_role_additional_policies = {
         ecr_read = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
