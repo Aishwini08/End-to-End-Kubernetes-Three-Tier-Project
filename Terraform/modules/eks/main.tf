d78844456d6a647db22a547e0ab352d4bc7362e8
Aishwini <aishwinipriyapillai@gmail.com>
2026-09-20 15:49:42 +0530

    Testing 2

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -17,8 +17,8 @@ module "eks" {
   eks_managed_node_groups = {
     worker-nodes = {
       instance_types = ["t3.large"]
-      min_size       = 1
-      max_size       = 2
+      min_size       = 2
+      max_size       = 5
       desired_size   = 2
 
       node_repair_config = {
